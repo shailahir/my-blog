f3e7b4deaa648aa7fe30d41c7edb19a9bd8d8a6a
Shailendra Ahir <shailendraahir@hotmail.com>
2025-08-18 01:35:57 +0530

    First commit

---
+++ src/test/java/com/shailahir/apps/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shailahir.apps.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
